8deda854c407150019048d1d79ac2a870df4bdd2
MihailLovch <90479976+MihailLovch@users.noreply.github.com>
2023-05-01 17:02:46 +0300

    init

---
+++ src/main/java/ru/kpfu/itis/semesterprojectspring/SemesterProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.semesterprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemesterProjectSpringApplication.class, args);
+    }
+
+}
